27a003c0cfaf37f5f377e7dd492ed5434f377726
wellpardim <wellingtons_10@hotmail.com>
2015-02-18 17:31:42 -0200

    bash simple script finished

--- BashTruthTable.sh
+++ BashTruthTable.sh
@@ -15,23 +15,30 @@ do
  
     if [ $n == 1 ]
     then
-      a="$a\"&&\""{0..1}
+      a="$a\"&&\"{0..1}"
       echo $a
     fi
     
     if [ $n == 2 ]
     then
-      a="$a\"||\""{0..1}
+      a="$a\"||\"{0..1}"
       echo $a
     fi
   
   fi
 done
 
- 
-www="for i in  $a"
-echo $www
-eval "$www ; do let \"result = (($i))\" ; echo \"$result\"; done"
+for i in $(eval "echo $a"); do let "result = (($i))"; echo $i = "$result"; done
+
+
+#printf -v a  "%d"   {0..1}{0..1}
+#for i in $a
+#do 
+#let "c = (($i))"
+#echo "$i"
+#done
+#echo $www
+#eval "$www ; do let \"result = (($i))\" ; echo \"$result\"; done"
 
 #echo $command
 #eval $command
